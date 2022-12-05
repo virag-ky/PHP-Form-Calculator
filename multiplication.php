99427f5a4b0dada7d5d47f75738e15f646b0ae40
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-05 17:19:27 +0200

    Add multiplication functionality

--- multiplication.php
+++ multiplication.php
@@ -4,6 +4,7 @@
     <title>Multiplication</title>
   </head>
   <body>
+    <?="<p>The result of ${_GET["multiply_num1"]} and ${_GET["multiply_num2"]} is " . ($_GET["multiply_num1"] * $_GET["multiply_num2"]) . ".</p>"?>
    <a href="index.php">Reset</a>
   </body>
 </html>
