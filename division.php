31a7439a9d3bc5e404955f41741864fa9cabd74d
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-05 17:21:46 +0200

    Add division functionality

--- division.php
+++ division.php
@@ -4,6 +4,7 @@
     <title>Division</title>
   </head>
   <body>
+    <?="<p>The quotient of ${_GET["dividend"]} and ${_GET["divisor"]} is " . ($_GET["dividend"] / $_GET["divisor"]) . ".</p>"?>
    <a href="index.php">Reset</a>
   </body>
 </html>
